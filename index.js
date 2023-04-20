b02d006cc9ff54ed12a728a1d33d3dabb38e4985
Ale-zand <ale_zand@hotmail.com>
2023-04-20 17:40:29 -0300

    regular expressions, regex, map e spreadoperator

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ import fs from 'fs';
 import chalk from 'chalk';
 // const chalk = require('chalk'); //forma antiga
 
+function extraiLinks(texto) {
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+    const capturas = [...texto.matchAll(regex)];
+    const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}))
+    return resultados;
+}
+
 function trataErro(erro) {
     console.log(erro);
     throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
@@ -13,7 +20,9 @@ async function pegaArquivo(caminhoDoArquivo) {
     try{
         const encoding = 'utf-8';
         const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
-        console.log(chalk.green(texto));
+        // extraiLinks(texto);
+        console.log(extraiLinks(texto));
+        // console.log(chalk.green(texto));
     } catch (erro) {
         trataErro(erro)
     }
@@ -41,4 +50,8 @@ async function pegaArquivo(caminhoDoArquivo) {
 // }
 
 pegaArquivo('./arquivos/texto.md');
-pegaArquivo('./arquivos/');
+// pegaArquivo('./arquivos/');
+
+// \[[^[\]]*?\]
+// \(https?:\/\/[^\s?#.].[^\s]*\)
+// \[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)
